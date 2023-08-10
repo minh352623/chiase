9c369dace98555adb0f53023cbf18af668d42533
minh352623 <congminh352623@gmail.com>
2023-08-10 21:18:08 +0700

    host fe

--- src/routers/authRoute.js
+++ src/routers/authRoute.js
@@ -14,7 +14,7 @@ const {
   online_user_id,
   online_user_email,
 } = require("../Middeware/AuthMiddleware");
-const CLIENT_URL = "http://localhost:5173";
+const CLIENT_URL = "https://lively-lamington-a39686.netlify.app/";
 // router.get("/logout", [isAuthentication], logout);
 
 router.get("/login-google-success/:id", [online_user_id], createTokenGoogle);
