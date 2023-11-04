43bb3a76c43cb2daac50ba755d514f336d0bf571
minh352623 <congminh352623@gmail.com>
2023-11-04 15:45:55 +0700

    change api conversation

--- src/services/ConversationService.js
+++ src/services/ConversationService.js
@@ -55,7 +55,27 @@ const getConversationService = async (req, res) => {
         },
       ],
     });
-    return res.status(200).send(conversations);
+    const newData = await Promise.all(conversations.map(async (item) => {
+      let id_user_conversation = 0;
+    
+      if (item.user_one != req.userId) {
+        id_user_conversation = item.user_one;
+      } else {
+        id_user_conversation = item.user_second;
+      }
+    
+      console.log("🚀 ~ file: ConversationService.js:61 ~ conversations.forEach ~ id_user_conversation:", id_user_conversation);
+    
+      item.dataValues.userConversation = await db.user.findOne({
+        where: {
+          id: id_user_conversation
+        }
+      });
+    
+      console.log("🚀 ~ file: ConversationService.js:75 ~ conversations.forEach ~ item:", item.dataValues);
+      return item;
+    }));
+    return res.status(200).send(newData);
   } catch (e) {
     console.log(e);
     return res.status(500).send(e);
